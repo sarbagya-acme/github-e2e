3a63f0a7766015f799488c9ad915949bbbc653d9
sarbagyastha <sarbagyastha@gmail.com>
2022-05-05 17:58:18 +0545

    :green_heart:

---
+++ ios/RunnerTests/RunnerTests.m
@@ -0,0 +1,11 @@
+//
+//  RunnerTests.m
+//  RunnerTests
+//
+//  Created by Sarbagya Dhaubanjar on 04/05/2022.
+//
+
+@import XCTest;
+@import integration_test;
+
+INTEGRATION_TEST_IOS_RUNNER(RunnerTests)
