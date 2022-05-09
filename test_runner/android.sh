9379625f1930673ca81f1077ac71cf8971673d76
sarbagyastha <sarbagyastha@gmail.com>
2022-05-09 15:42:10 +0545

    :robot:

--- test_runner/android.sh
+++ test_runner/android.sh
@@ -8,4 +8,5 @@ gcloud firebase test android run --type instrumentation \
   --app build/app/outputs/apk/debug/app-debug.apk \
   --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
   --use-orchestrator \
-  --timeout 3m
+  --timeout 3m \
+  --device-ids=Pixel3
